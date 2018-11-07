5ad29248347bde82a4ea0112bbeca25bc4564b13
Hoanglvph05611 <hoanglvph05611@fpt.edu.vn>
2018-11-06 19:50:57 -0800

    ass

--- app/src/main/java/com/example/lvhco/assigment/MainActivity.java
+++ app/src/main/java/com/example/lvhco/assigment/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.lvhco.assigment;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1500);
+                } catch (Exception e){
+
+                }finally {
+                    Intent intent = new Intent(MainActivity.this,Login.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        thread.start();
     }
 }
